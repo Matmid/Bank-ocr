74033a1b6b6312934537b1a91ea9820e8b794442
Matthew Middleditch <matthew.middleditch@sage.com>
2015-09-18 16:28:33 +0100

    implemented pending tests for validator_spec.rb

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
+require 'validator'
 
 describe Validator do
+
   context 'checksum' do
-    it 'works out the correct value for checksum calculation'
+  let(:valid) {"123456789"}
+  let(:invalid) {"888888888"}
 
-    it 'returns true if account number is valid'
+    it 'returns true if account number is valid' do
+      expect(subject.checksum(valid)).to eq(true)
+    end
 
-    it 'returns false if account number is invalid'
+    it 'returns false if account number is invalid' do
+      expect(subject.checksum(invalid)).to eq(false)
+    end
 
   end
 end
